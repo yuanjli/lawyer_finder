2a0fb6edb5c1df9defbcdc0a074258640c20dd06
yuanjli <liyuanjienetwork@gmail.com>
2018-10-05 11:33:40 -0700

    added style.css and animate.css

--- finderApp/admin.py
+++ finderApp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
+admin.AdminSite.site_header = 'My Awesome finder Admin Panel!'
 
 # Register your models here.
+
+admin.site.register(models.Todo)
+admin.site.register(models.Question)
+admin.site.register(models.Choice)
