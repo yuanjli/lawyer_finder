3215fcca8e8f7c41e28931a115cc51e879161299
DeusMalsith <justinrashallphotography@gmail.com>
2018-10-05 14:15:26 -0700

    add

--- finderApp/urls.py
+++ finderApp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.signup, name="signup"),
-    url(r'^index', views.index, name="index"),
+    url(r'^index$', views.index, name="index"),
+    url(r'^index2$', views.index2, name="index2"),
     url(r'^login$', views.login, name="login"),
     url(r'^logout$', views.logout, name="logout"),
     url(r'^(?P<todo_id>[0-9]+)/done/$', views.done, name='done'),
